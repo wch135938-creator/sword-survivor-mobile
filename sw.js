52b04dbc50c77480814300c64a8641f9fbce1cb4
Vwa <wch135938@gmail.com>
2026-07-20 15:40:09 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-const CACHE='sword-survivor-v2.2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE)));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
+const CACHE='sword-survivor-v2.3';
