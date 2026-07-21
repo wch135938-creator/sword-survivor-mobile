f0a536143e66b9fde8015fbacb15d8f64338f723
Vwa <wch135938@gmail.com>
2026-07-21 19:36:52 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-const RELEASE = "sword-survivor-v2.7.1-fix2";
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request, { cache: "no-store" }));
-});
+const CACHE='sword-survivor-v2.7.3';
